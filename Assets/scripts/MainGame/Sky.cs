029a907d392c4b9f7065a4fca66b2f4755e67254
yewon0804 <ehdnjs0804@gmail.com>
2022-06-27 14:07:24 +0900

    메인 게임 배경 리소스 변경 (#1)
    Title 화면과 동일한 Skydom 에셋으로 변경

--- Assets/scripts/MainGame/Sky.cs
+++ Assets/scripts/MainGame/Sky.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Sky : MonoBehaviour
 {
 
-    float speed = 0.02f;
+    float speed = 0.001f;
     // Update is called once per frame
     void Update()
     {
         float ofs = speed * Time.time;
-        transform.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(ofs, -1);
+        // transform.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(ofs, -1);
+        transform.Rotate(new Vector3(0, ofs, 0));
     }
 }
